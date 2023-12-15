d91c18babb8f1ece272db1c8d7b4e60ae385407e
Marcus Amaral Rolim <m4rkux@gmail.com>
2023-12-15 02:40:07 -0800

    Add hanoi game base

--- src/routes/minigame/hanoi/+page.svelte
+++ src/routes/minigame/hanoi/+page.svelte
@@ -1,23 +1,56 @@
 <script lang="ts">
   const disks = [...new Array(6).keys()];
-  const poles = [
+  let poles = [
     [...disks],
     [],
     []
   ];
+  let currentDisk: number = -1;
+  let hoverPole: number = 0;
+
+  function handleClickDisk(poleIndex: number) {
+    if (currentDisk < 0) {
+      currentDisk = poles[poleIndex].shift() as number;
+    }
+    poles = poles;
+    hoverPole = poleIndex;
+  }
+  
+  function handleClickPole(poleIndex: number) {
+    if (currentDisk > -1 && (!poles[poleIndex].length || currentDisk < poles[poleIndex][0])) {
+      poles[poleIndex].unshift(currentDisk);
+      currentDisk = -1;
+    }
+    poles = poles;
+  }
+
+  function handleHoverPole(poleIndex: number) {
+    hoverPole = poleIndex;
+  }
 </script>
 
-<div class="flex flex-col align-middle items-center w-[210px]">
-  {#each poles as poleDisks}
-    {#each poleDisks as disk, i}
-      <div class="disk disk-{disk} {i === 0 && 'grabbable'}"></div>
+<div class="w-full flex">
+  <div class="mx-auto flex flex-row relative">
+    {#if currentDisk > -1}
+      <div class="w-[230px] floating floating-{hoverPole}">
+        <button class="disk disk-{currentDisk}" />
+      </div>
+    {/if}
+    {#each poles as poleDisks, poleIndex}
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div class="flex flex-col justify-end items-center w-[230px] h-[230px] border-b-2 border-black pt-4 px-auto relative" on:mouseenter={() => handleHoverPole(poleIndex)}>
+        <button class="pole {poleIndex === poles.length - 1 && 'pole-goal'} {currentDisk > -1 && 'clickable'}" on:click={() => handleClickPole(poleIndex)} />
+        {#each poleDisks as disk, i}
+          <button class="disk disk-{disk} {currentDisk === -1 && i === 0 && 'clickable'}" on:click={() => handleClickDisk(poleIndex)} />
+        {/each}
+      </div>
     {/each}
-  {/each}
+  </div>
 </div>
 
 <style lang="scss">
   .disk {
-    @apply block h-[24px] rounded-lg border-black border-solid border-[1px];
+    @apply block h-[24px] rounded-lg border-black border-solid border-[1px] z-10 cursor-default transition-transform;
 
     &-0 {
       @apply bg-purple-700 w-[60px];
@@ -44,15 +77,49 @@
     }
   }
 
-  .grabbable {
-    @apply cursor-grab;
+  .floating {
+    @apply absolute flex justify-center transition-all;
+
+    button {
+      @apply shadow-[0_0_2px_1px];
+    }
+
+    &-0 {
+      @apply translate-x-0;
+    }
+    &-1 {
+      @apply translate-x-[230px];
+    }
+    &-2 {
+      @apply translate-x-[460px];
+    }
+  }
+
+  .pole {
+    @apply bg-amber-800 w-[20px] h-[160px] block rounded-t-lg border-black border-solid border-[1px] absolute z-0 bottom-0 cursor-default;
+
+    &-goal {
+      @apply bg-yellow-700;
+    }
+  }
+
+  .clickable {
+    @apply cursor-pointer;
+
+    &.disk:hover {
+      @apply -translate-y-px rotate-3;
+    }
+
+    &.pole:hover {
+      @apply shadow-[0_-3px_2px_2px];
+    }
 
     &:hover {
       @apply shadow-[0_0_2px_2px] shadow-sky-100;
     }
 
     &:active {
-      @apply cursor-grabbing shadow-[0_0_2px_1px] shadow-sky-100;
+      @apply shadow-[0_0_2px_1px] shadow-sky-100;
     }
   }
 </style>
